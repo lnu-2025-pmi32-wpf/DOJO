724f3f5ef45f6dbe71a55a80a88a2b633c29b7ad
Anna Dzhavala <annadzavala@gmail.com>
2025-12-09 20:23:38 +0200

    fixes

--- dojo/Presentation/Views/AddPlanPage.xaml.cs
+++ dojo/Presentation/Views/AddPlanPage.xaml.cs
@@ -9,6 +9,26 @@ namespace Presentation.Views
             InitializeComponent();
             BindingContext = viewModel;
         }
+
+        private void OnStartDateTapped(object sender, EventArgs e)
+        {
+            StartDatePicker.Focus();
+        }
+
+        private void OnStartTimeTapped(object sender, EventArgs e)
+        {
+            StartTimePicker.Focus();
+        }
+
+        private void OnEndDateTapped(object sender, EventArgs e)
+        {
+            EndDatePicker.Focus();
+        }
+
+        private void OnEndTimeTapped(object sender, EventArgs e)
+        {
+            EndTimePicker.Focus();
+        }
     }
 }
 
